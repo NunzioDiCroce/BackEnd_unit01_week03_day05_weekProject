2fab498fcea32b207723f09792d011a4b5e5ccf5
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-14 11:45:48 +0200

    Update Libro.java

--- src/main/java/gestioneCatalogo/Libro.java
+++ src/main/java/gestioneCatalogo/Libro.java
@@ -1,5 +1,51 @@
 package gestioneCatalogo;
 
-public class Libro {
+public class Libro extends Elemento {
 
+	// - - - - - - - - - - - - - - - - - - - - attributes
+	protected String autore;
+	protected String genere;
+
+	// - - - - - - - - - - - - - - - - - - - - constructors
+	public Libro(String _isbn, String _titolo, int _anno, int _pagine) {
+		super(_isbn, _titolo, _anno, _pagine);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Libro(String _isbn, String _titolo, int _anno, int _pagine, String _autore, String _genere) {
+		super(_isbn, _titolo, _anno, _pagine);
+		this.autore = _autore;
+		this.genere = _genere;
+	}
+
+	// - - - - - - - - - - - - - - - - - - - - getters & setters
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public int getAnno() {
+		return anno;
+	}
+
+	public int getPagine() {
+		return pagine;
+	}
+
+	public String getAutore() {
+		return autore;
+	}
+
+	public String getGenere() {
+		return genere;
+	}
+
+	@Override
+	public String toString() {
+		return "[ISBN " + isbn + " " + "\"" + titolo + "\"" + ", " + anno + ", " + pagine + " pagine" + ", autore "
+				+ autore + ", genere " + genere + "]";
+	}
 }
