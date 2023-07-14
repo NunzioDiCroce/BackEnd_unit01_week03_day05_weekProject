91eed3509d394900d08f873ba181f70c09d96e96
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-14 14:12:56 +0200

    Update App.java

--- src/main/java/gestioneCatalogo/App.java
+++ src/main/java/gestioneCatalogo/App.java
@@ -1,5 +1,31 @@
 package gestioneCatalogo;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import utils.JpaUtil;
+
 public class App {
+
+	private static EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+
+	public static void main(String[] args) {
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		System.out.println("");
+		System.out.println(
+				"- - - - - - - - - - - - - - - App Gestione Catalogo is running - - - - - - - - - - - - - - -");
+
+		// - - - - - - - - - - - - - - - - - - - - OBJECTS CREATION
+
+		// - - - - - - - - - - - - - - - - - - - - SETTING & SAVING
+
+		// - - - - - - - - - - - - - - - - - - - - entityManager & entityManagerFactory
+		// closing
+		entityManager.close();
+		entityManagerFactory.close();
+
+	}
 
 }
