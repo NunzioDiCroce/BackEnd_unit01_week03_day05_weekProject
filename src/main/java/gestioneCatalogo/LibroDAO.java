673f8040de18eb8711239b0cf2f0d35478e54b10
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-14 14:01:58 +0200

    Create LibroDAO.java

--- src/main/java/gestioneCatalogo/LibroDAO.java
+++ src/main/java/gestioneCatalogo/LibroDAO.java
@@ -1,5 +1,40 @@
 package gestioneCatalogo;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 public class LibroDAO {
+
+	private final EntityManager entityManager;
+
+	public LibroDAO(EntityManager _entityManager) {
+		this.entityManager = _entityManager;
+	}
+
+	public void save(Libro _libro) {
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(_libro);
+		entityTransaction.commit();
+		System.out.println("Libro salvato correttamente");
+	}
+
+	public Libro findById(long _id) {
+		Libro libroCercato = entityManager.find(Libro.class, _id);
+		return libroCercato;
+	}
+
+	public void findByIdAndDelete(long _id) {
+		Libro libroCercato = entityManager.find(Libro.class, _id);
+		if (libroCercato != null) {
+			EntityTransaction entityTransaction = entityManager.getTransaction();
+			entityTransaction.begin();
+			entityManager.remove(libroCercato);
+			entityTransaction.commit();
+
+		} else {
+			System.out.println("Libro non trovato");
+		}
+	}
 
 }
