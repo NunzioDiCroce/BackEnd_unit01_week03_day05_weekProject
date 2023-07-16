fc3c5306c6df7fd3a049612d17d185a5ed959497
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-16 17:19:01 +0200

    Update App.java

--- src/main/java/gestioneCatalogo/App.java
+++ src/main/java/gestioneCatalogo/App.java
@@ -56,102 +56,105 @@ public class App {
 		PrestitoDAO prestitoDao = new PrestitoDAO(entityManager);
 
 		// - - - - - - - - - - - - - - - - - - - - GESTIONE UTENTI E PRESTITI
-		// utenteDao.save(primoUtente);
-		// utenteDao.save(secondoUtente);
-		// utenteDao.save(terzoUtente);
-		// prestitoDao.save(primoPrestito);
-		// prestitoDao.save(secondoPrestito);
-		// prestitoDao.save(terzoPrestito);
-		// prestitoDao.save(quartoPrestito);
-		// prestitoDao.save(quintoPrestito);
+//		 utenteDao.save(primoUtente);
+//		 utenteDao.save(secondoUtente);
+//		 utenteDao.save(terzoUtente);
+//		 prestitoDao.save(primoPrestito);
+//		 prestitoDao.save(secondoPrestito);
+//		 prestitoDao.save(terzoPrestito);
+//		 prestitoDao.save(quartoPrestito);
+//		 prestitoDao.save(quintoPrestito);
 
 		// - - - - - - - - - - - - - - - - - - - - 1) Aggiunta di un elemento del
 		// catalogo
-		// libroDao.save(primoLibro);
-		// libroDao.save(secondoLibro);
-		// libroDao.save(terzoLibro);
-		// rivistaDao.save(primaRivista);
-		// rivistaDao.save(secondaRivista);
+//		libroDao.save(primoLibro);
+//		libroDao.save(secondoLibro);
+//		libroDao.save(terzoLibro);
+//		rivistaDao.save(primaRivista);
+//		rivistaDao.save(secondaRivista);
 
-		// * * * * * * * * * * * * * * * * * * * * LIBRO * * * * * * * * * * * * * * * *
-		// * * * *
-		// - - - -
 		// - - - - - - - - - - - - - - - - - - - - 2) Rimozione di un elemento del
 		// catalogo dato un codice ISBN
-		// libroDao.rimuoviPerISBN("001");
+//		libroDao.rimuoviPerISBN("001");
+//		rivistaDao.rimuoviPerISBN("010");
 
 		// - - - - - - - - - - - - - - - - - - - - 3) Ricerca per ISBN
-		// libroDao.cercaPerISBN("001");
+		System.out.println("");
+		System.out.println("- - - - - - - - - - - - - - - - - - - - 3) Ricerca per ISBN");
+		libroDao.cercaPerISBN("002");
 
-//		Libro libroCercato = libroDao.cercaPerISBN("001");
-//
-//		if (libroCercato != null) {
-//			System.out.println(libroCercato.toString());
-//		} else {
-//			System.out.println("Nessun libro trovato per l'ISBN specificato");
-//		}
-//
-//		// - - - - - - - - - - - - - - - - - - - - 4) Ricerca per anno pubblicazione
-//		List<Libro> libriPerAnno = libroDao.cercaPerAnno(1980);
-//
-//		if (!libriPerAnno.isEmpty()) {
-//			for (Libro libro : libriPerAnno) {
-//				System.out.println(libro.toString());
-//			}
-//		} else {
-//			System.out.println("Nessun libro trovato per l'anno specificato");
-//		}
-//
-//		// - - - - - - - - - - - - - - - - - - - - 5) Ricerca per autore
-//
-//		List<Libro> libriPerAutore = libroDao.cercaPerAutore("Mario Rossi");
-//
-//		if (!libriPerAutore.isEmpty()) {
-//			for (Libro libro : libriPerAutore) {
-//				System.out.println(libro.toString());
-//			}
-//		} else {
-//			System.out.println("Nessun libro trovato per l'autore specificato");
-//		}
-//
-//		// - - - - - - - - - - - - - - - - - - - - 5) Ricerca per titolo o parte di esso
-//		List<Libro> libriPerTitolo = libroDao.cercaPerTitolo("primo");
-//
-//		if (!libriPerTitolo.isEmpty()) {
-//			for (Libro libro : libriPerTitolo) {
-//				System.out.println(libro.toString());
-//			}
-//		} else {
-//			System.out.println("Nessun libro trovato per il titolo specificato");
-//		}
+		Libro libroCercato = libroDao.cercaPerISBN("002");
+
+		if (libroCercato != null) {
+			System.out.println(libroCercato.toString());
+		} else {
+			System.out.println("Nessun libro trovato per l'ISBN specificato");
+		}
+
+		rivistaDao.cercaPerISBN("020");
+
+		Rivista rivistaCercata = rivistaDao.cercaPerISBN("020");
+
+		if (rivistaCercata != null) {
+			System.out.println(rivistaCercata.toString());
+		} else {
+			System.out.println("Nessuna rivista trovata per l'ISBN specificato");
+		}
+
+		// - - - - - - - - - - - - - - - - - - - - 4) Ricerca per anno pubblicazione
+		System.out.println("");
+		System.out.println("- - - - - - - - - - - - - - - - - - - - 4) Ricerca per anno pubblicazione");
+		java.util.List<Libro> libriPerAnno = libroDao.cercaPerAnno(2000);
+
+		if (!libriPerAnno.isEmpty()) {
+			for (Libro libro : libriPerAnno) {
+				System.out.println(libro.toString());
+			}
+		} else {
+			System.out.println("Nessun libro trovato per l'anno specificato");
+		}
+
+		java.util.List<Rivista> rivistePerAnno = rivistaDao.cercaPerAnno(2020);
+
+		if (!rivistePerAnno.isEmpty()) {
+			for (Rivista rivista : rivistePerAnno) {
+				System.out.println(rivista.toString());
+			}
+		} else {
+			System.out.println("Nessuna rivista trovata per l'anno specificato");
+		}
+
+		// - - - - - - - - - - - - - - - - - - - - 5) Ricerca per autore
+		System.out.println("");
+		System.out.println("- - - - - - - - - - - - - - - - - - - - 5) Ricerca per autore");
+		List<Libro> libriPerAutore = libroDao.cercaPerAutore("Mario Rossi");
+
+		if (!libriPerAutore.isEmpty()) {
+			for (Libro libro : libriPerAutore) {
+				System.out.println(libro.toString());
+			}
+		} else {
+			System.out.println("Nessun libro trovato per l'autore specificato");
+		}
+
+		// - - - - - - - - - - - - - - - - - - - - 5) Ricerca per titolo o parte di esso
+		List<Libro> libriPerTitolo = libroDao.cercaPerTitolo("primo");
+
+		if (!libriPerTitolo.isEmpty()) {
+			for (Libro libro : libriPerTitolo) {
+				System.out.println(libro.toString());
+			}
+		} else {
+			System.out.println("Nessun libro trovato per il titolo specificato");
+		}
 
 		// * * * * * * * * * * * * * * * * * * * * RIVISTA * * * * * * * * * * * * * * *
 		// * * * *
 		// - - - - - - - - - - - - - - - - - - - - 2) Rimozione di un elemento del
 		// catalogo dato un codice ISBN
-		// rivistaDao.rimuoviPerISBN("001");
 
 		// - - - - - - - - - - - - - - - - - - - - 3) Ricerca per ISBN
-		// rivistaDao.cercaPerISBN("001");
 
-//				Rivista rivistaCercata = rivistaDao.cercaPerISBN("001");
-		//
-//				if (rivistaCercata != null) {
-//					System.out.println(rivistaCercata.toString());
-//				} else {
-//					System.out.println("Nessuna rivista trovata per l'ISBN specificato");
-//				}
-		//
-//				// - - - - - - - - - - - - - - - - - - - - 4) Ricerca per anno pubblicazione
-//				List<Rivista> rivistePerAnno = rivistaDao.cercaPerAnno(1980);
-		//
-//				if (!rivistePerAnno.isEmpty()) {
-//					for (Rivista rivista : rivistePerAnno) {
-//						System.out.println(rivista.toString());
-//					}
-//				} else {
-//					System.out.println("Nessuna rivista trovata per l'anno specificato");
-//				}
 		//
 //				// - - - - - - - - - - - - - - - - - - - - 5) Ricerca per autore
 		//
